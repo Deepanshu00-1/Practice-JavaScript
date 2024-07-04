b07de8f31ed1224984805d15504087f11e019617
Deepanshu <krrohan129@example.com>
2024-07-03 17:56:27 -0700

    practiced date,time

---
+++ Dom in js/selectors.js
@@ -0,0 +1,9 @@
+// 1 getElementById
+const newText = document.getElementById('text');
+
+newText.textContent = "Hello there!"
+
+// 2. querySelector
+const anotherText = document.querySelector('.anotherText');
+anotherText.innerHTML = "How are you"
+
